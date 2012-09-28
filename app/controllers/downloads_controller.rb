48fae42d9b81d9d52212e35a0943e941151b2a69
xiaofei li <xiaofei.li@sidways.com>
2012-09-28 11:44:35 +0800

    download images

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -4,9 +4,14 @@ class DownloadsController < ApplicationController
   http_basic_authenticate_with :name => "media.cooper", :password => "cooperchina", :realm => "Cooper"
 
   def index
-    @files = Refinery::Resource.where("media_type = '新闻通稿'")
-    @images_poster =  Refinery::Resource.where("media_type = '高清图片' AND image_type = '海报'")
-    @images_foldout = Refinery::Resource.where("media_type = '高清图片' AND image_type = '折页'")    
-    @movies = Video.where("media_type = '视频'")
+    # @files = Refinery::Resource.where("media_type = '新闻通稿'")
+    # @images_poster =  Refinery::Resource.where("media_type = '高清图片' AND image_type = '海报'")
+    # @images_foldout = Refinery::Resource.where("media_type = '高清图片' AND image_type = '折页'")    
+    # @movies = Video.where("media_type = '视频'")
+
+    if params[:file].present?
+    	send_file(Rails.root.join('public/images', params[:file]))
+    end
   end
+ 
 end

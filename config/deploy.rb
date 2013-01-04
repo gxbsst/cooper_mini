ab5cc31e2ce5f7d663cef64224786f8bd7d46f0f
Weston <weixuhong@gmail.com>
2013-01-04 16:39:42 +0800

    update gem file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,37 @@
 require "bundler/capistrano"
 
-server "180.168.220.98", :web, :app, :db, primary: true
+# Add RVM's lib directory to the load path.
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#set :bundle_cmd, 'source $HOME/.bash_profile && bundle'
+# Load RVM's capistrano plugin.    
+# require "rvm/capistrano"
 
-set :application, "Cooper"
-set :repository,  "git@git.sidways.lab:ruby/outsourcing/cooper"
-set :deploy_to, "/srv/rails/cooper"
+#set :rvm_ruby_string, '1.9.2'
+#set :rvm_type, :user  # Don't use system-wide RVM
+#set :rvm_type, :user
+
+
+set :deploy_via, :remote_cache
+
+set :application, "Mctires"
+
+#set :default_environment, {
+  #'LANG' => 'en_US.UTF-8'
+#}
+
+if ENV['RAILS_ENV'] =='production'
+  #server "www.coopertire.com.cn", :web, :app, :db, primary: true
+  server "jh_web3", :web, :app, :db, primary: true
+  set :repository,  "git://github.com/gxbsst/cooper_mini.git"
+ # set :deploy_to, "/srv/rails/coopertire_deploy"
+  set :deploy_to, "/srv/rails/mctires"
+  set :user, "root"
+else
+  server "rails", :web, :app, :db, primary: true
+  set :repository,  "git://github.com/gxbsst/cooper_mini.git"
+  set :deploy_to, "/srv/rails/mctires"
+  set :user, "rails" 
+end
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -14,16 +41,17 @@ set :scm, :git
 # role :db,  "aries.sidways.lab", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
-set :user, "rails"
+#set :user, "rails"
+# set :user, "root"
 
 set :use_sudo, false
 
-# set :branch, "master"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+#after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 
 # if you're still using the script/reaper helper you will need
@@ -36,21 +64,35 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
- end
- 
+
  task :setup_config, roles: :app do
    # sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
    run "mkdir -p #{shared_path}/config"
    put File.read("config/database.yml.mysql"), "#{shared_path}/config/database.yml"
    puts "Now edit the config files in #{shared_path}."
+   # photos
+   # run "mkdir -p /srv/rails/coopertire_stuff/system"
  end
  
  after "deploy:setup", "deploy:setup_config"
- 
+
  task :symlink_config, roles: :app do
    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+   #   run "ln -nfs #{shared_path}/config/database.yml  /srv/rails/cooper/releases/20121205032322/config/database.yml"
+   # run "ln -nfs /srv/rails/coopertire_stuff/system #{release_path}/public/system"
  end
  after "deploy:finalize_update", "deploy:symlink_config"
+
+ task :bundle_install do 
+   run("cd #{deploy_to}/current; bundle install --path=vendor/gems")
+ end
+
+ task :migration do 
+   run("cd #{deploy_to}/current; rake db:migrate ")
+ end
+
+ end
+ 
  
  # 设置数据库
  # namespace :deploy do
@@ -59,15 +101,3 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
  #   end
  # end
  
- namespace :deploy do
-   task :bundle_install do 
-      run("cd #{deploy_to}/current; bundle install --path=vendor/gems")
-   end
- end
- 
- 
- namespace :deploy do
-   task :migration do 
-      run("cd #{deploy_to}/current; rake db:migrate ")
-   end
- end

c1dc9c2f2e17196783e140f243386f738f662704
gxbsst <gxbsst@gmail.com>
2013-04-26 17:25:26 +0800

    Sort Decorative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sort_decorative
+    #Product.name_all_collection
+     [
+      ["MC-440", "MC-440"],
+      ["AVENGER-HP", "AVENGER-HP"],
+      ["STRATEGY", "STRATEGY"],
+      ["COURSER AXT", "COURSER AXT"],
+      ["COURSER CT", "COURSER CT"], 
+      ["COURSER MT", "COURSER MT"]
+    ]
+  end
 end
